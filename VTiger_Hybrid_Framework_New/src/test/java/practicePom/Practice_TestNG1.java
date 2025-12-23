4533bd9917e847b1006ec15ddb7a3540318f1801
pc-pc <pc-pc@DESKTOP-4TLMG95>
2025-12-23 18:19:23 +0500

    createContactWithOrgTest CREATED

--- VTiger_Hybrid_Framework_New/src/test/java/practicePom/Practice_TestNG1.java
+++ VTiger_Hybrid_Framework_New/src/test/java/practicePom/Practice_TestNG1.java
@@ -2,6 +2,12 @@ package practicePom;
 
 import org.testng.annotations.Test;
 
+import objectRepositoryUtility.Contact_Page;
+import objectRepositoryUtility.Contact_createNewCon_Page;
+import objectRepositoryUtility.HomePage;
+import objectRepositoryUtility.Org_Page;
+import objectRepositoryUtility.Org_createNewOrg_Page;
+
 public class Practice_TestNG1 extends PracticeBaseClass{
 	
 	@Test
@@ -41,4 +47,49 @@ public class Practice_TestNG1 extends PracticeBaseClass{
 	public void orgWithIndustries() {
 		System.out.println("create orgWithIndustries");
 	}
+	
+	
+public void createContactWithOrgTest() throws Throwable {
+
+		
+
+		HomePage hp = new HomePage(driver);
+
+		hp.getOrgLink().click();;
+
+		Org_createNewOrg_Page ocp = new Org_createNewOrg_Page(driver);
+
+		Org_Page op = new Org_Page(driver);
+
+		op.getCreateNewOrgIcon().click();
+
+		String orgName = eLib.getDataFRomExcel("contact", 4, 2)+jLib.getRandomNumber();
+
+		ocp.createOrganization(orgName);
+
+		Thread.sleep(2000);
+
+		hp.getContactLink().click();
+
+		Contact_Page cp = new Contact_Page(driver);
+
+		cp.getCreateContactIcon().click();
+
+		String firstName = eLib.getDataFRomExcel("contact", 1, 2);
+
+		String lastName = eLib.getDataFRomExcel("contact", 1, 3);
+
+		String supportStartDate= jLib.getSystemDateyyyyMMdd();
+
+		String supportEndDate = jLib.getRequiredDateyyyyMMdd(30);
+
+		Contact_createNewCon_Page cc = new Contact_createNewCon_Page(driver);
+
+		cc.createContactwithDateOrg(orgName, firstName, lastName, supportStartDate, supportEndDate);
+
+		
+
+		}
+
+
 }
